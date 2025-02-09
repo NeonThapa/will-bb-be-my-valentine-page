588c198e3c74799ac00a47121c3b10f27be0b544
neotros <neonthapa@gmail.com>
2025-02-09 14:29:19 +0530

    pic there

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  base: '/', // ADD THIS LINE
   plugins: [react()],
+  base: '/will-bb-be-my-valentine-page/',
+  build: {
+    minify: 'esbuild', // Change to 'esbuild'
+    sourcemap: false,
+  },
 })
